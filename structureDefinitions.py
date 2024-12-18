4b16bcac6d89b29185fedb0f5e70f7e1d410a3f8
Paweł Hase <pawelhase@gmail.com>
2024-12-18 18:03:30 +0100

    Dodanie funkcji layout_path

--- structureDefinitions.py
+++ structureDefinitions.py
@@ -1,9 +1,15 @@
 # images
 images_dir = "images/"
 
+
 # layouts
 layouts_dir = "layouts/"
 layout_ext = "-layout"
+
+def layout_path(path):
+    image_name = path.split("/")[-1].split(".")[0]
+    return layouts_dir + image_name + layout_ext
+
 
 # openCV window
 window_width, window_height = 1400, 900
