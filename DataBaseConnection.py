d8ca62c1157dc2d7b7fe9978373212c443eb3b8a
maciejl <maciej.lencewicz@gmail.com>
2025-01-26 20:23:27 +0100

    Dodanie metody checkEmployeePlace

--- DataBaseConnection.py
+++ DataBaseConnection.py
@@ -40,12 +40,39 @@ class DataBaseConnection:
 
         result = self.cursor.execute(query, (REG_PLATE_NO,))
         result = result.fetchall()
-
         if len(result) == 0:
             return False
 
         return True
 
+
+    def checkEmployeePlace(self, Parking_space_number, REG_PLATE_NO):
+        query = """
+            SELECT 
+                E.PARKING_SPACE_NO = ? AS is_correct
+            FROM 
+                CARS C
+            JOIN 
+                EMPLOYEES E 
+            ON 
+                C.ID_EMPLOYEE = E.ID_EMPLOYEE
+            WHERE 
+                C.REG_PLATE_NO = ?;
+            """
+
+        self.cursor.execute(query, (Parking_space_number, REG_PLATE_NO))
+        result = self.cursor.fetchone()
+
+        if result is None:
+            return None
+        return bool(result[0])
+
+
+
 db = DataBaseConnection()
 
 print(db.selectEmployeeCar("EL7ZABS"))
+
+print(db.checkEmployeePlace(1,"EL7ZABS"))
+
+
